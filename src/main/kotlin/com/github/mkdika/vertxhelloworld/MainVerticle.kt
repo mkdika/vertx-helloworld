0943b4ab242c16b588214d4de0f549dd46b98b24
Maikel Chandika <mkdika@gmail.com>
2020-02-19 14:01:57 +0700

    Refactor verticle process with some Promise (Future) flavor

--- src/main/kotlin/com/github/mkdika/vertxhelloworld/MainVerticle.kt
+++ src/main/kotlin/com/github/mkdika/vertxhelloworld/MainVerticle.kt
@@ -1,80 +1,70 @@
 package com.github.mkdika.vertxhelloworld
 
 import io.vertx.core.AbstractVerticle
-import io.vertx.core.Future
 import io.vertx.core.Promise
 import io.vertx.core.Vertx
 import io.vertx.redis.client.Redis
 import io.vertx.redis.client.RedisAPI
 import io.vertx.redis.client.RedisOptions
 
-
 class MainVerticle : AbstractVerticle() {
 
   override fun start(startPromise: Promise<Void>) {
     println("Start verticle...")
+
     val key = "abc"
-
-//    val redis = Redis.createClient(vertx, RedisOptions())
-//    redis.connect { r ->
-//      if (r.succeeded()) {
-//        val client = r.result()
-//        val api = RedisAPI.api(client)
-//        api.get(key) { result ->
-//          if (result.succeeded() && result.result() != null) {
-//            println("Result: abc:${result.result()}")
-//          }else {
-//            println("Result error: ${result.cause()}")
-//          }
-//        }
-//      }else {
-//        println("Result error: ${r.cause()}")
-//      }
-//    }
-
-
     val redisDao = RedisDao(vertx)
-    val operation = redisDao.getBacklog(key).future()
-    operation.setHandler {
-      if (it.succeeded()) {
-        println("Result: abc:${it.result()}")
-      } else {
-        println("Result error: ${it.cause()}")
+    redisDao.getBacklog(key)
+      .future()
+      .setHandler { result ->
+        if (result.succeeded()) {
+          println("Result: abc:${result.result()}")
+        } else {
+          println("Result error: ${result.cause()}")
+        }
       }
-    }
   }
 }
 
-class TestUsecase {
+class RedisApi(val redisClient: Redis) {
 
-  fun execute(id: Int): Future<String> {
+  private val redisApi: RedisAPI = RedisAPI.api(redisClient)
 
-    return Future.succeededFuture()
+  fun getRedisApi(): RedisAPI {
+    return redisApi
   }
 }
+
 
 class RedisDao(val vertx: Vertx) {
-
   fun getBacklog(key: String): Promise<String> {
 
-    val promisex = Promise.promise<String>()
-    val redis = Redis.createClient(vertx, RedisOptions())
-    redis.connect { r ->
-      if (r.succeeded()) {
-        val client = r.result()
-        val api = RedisAPI.api(client)
-        api.get(key) { result ->
-          if (result.succeeded() && result.result() != null) {
-            promisex.complete(result.result().toString())
-          }else {
-            promisex.fail(result.cause())
-          }
+    val redisPromise = Promise.promise<Redis>()
+    val resultPromise = Promise.promise<String>()
+    Redis.createClient(vertx, RedisOptions())
+      .connect { conn ->
+        if (conn.succeeded()) {
+          redisPromise.complete(conn.result())
+        } else {
+          redisPromise.fail(conn.cause())
         }
-      }else {
-        promisex.fail(r.cause())
       }
-    }
-    return promisex
+    redisPromise
+      .future()
+      .setHandler { redisClient ->
+        if (redisClient.succeeded()) {
+          val redisApi = RedisApi(redisClient.result()).getRedisApi()
+          redisApi.get(key) { result ->
+            if (result.succeeded()) {
+              resultPromise.complete(result.result().toString())
+            }else {
+              resultPromise.fail(result.cause())
+            }
+          }
+        } else {
+          resultPromise.fail(redisClient.cause())
+        }
+      }
+    return resultPromise
   }
 }
-
